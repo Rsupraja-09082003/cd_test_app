5c00959303ddfeccc0314b079e7b3028e0cebf00
Supraja R <159264304+Rsupraja-09082003@users.noreply.github.com>
2026-04-29 12:02:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "ML Model is running lets goooo  yayy y    yesss yayyyy lets gooooo!" 
+    return "ML Model is running lets goooo  yayy y    yesss yayyyy lets gooooo   yesssss!" 
 
 @app.route("/predict", methods=["POST"])
 def predict():
